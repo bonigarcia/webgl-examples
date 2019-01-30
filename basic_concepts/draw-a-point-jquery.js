f2ed74ade329bb3092865503426037e880bda72e
Boni Garcia <bgarcia@gsyc.es>
2019-01-30 11:12:02 +0100

    Drawing a point basic example with shaders in separate files using jQuery

---
+++ basic_concepts/draw-a-point-jquery.js
@@ -0,0 +1,61 @@
+    function init() {
+        // Get canvas object from the DOM
+        var canvas = document.getElementById("myCanvas");
+
+        // Init WebGL context
+        var gl = canvas.getContext("webgl");
+        if (!gl) {
+            console.log("Failed to get the rendering context for WebGL");
+            return;
+        }
+
+        // Read shaders using AJAX with jQuery
+        var vsSrc = document.getElementById('shaderVs').src;
+        $.get(vsSrc, function (vs) {
+            var fsSrc = document.getElementById('shaderFs').src;
+            $.get(fsSrc, function (fs) {
+                // Clear canvas
+                gl.clearColor(0.0, 0.0, 0.0, 1.0);
+                gl.clear(gl.COLOR_BUFFER_BIT);
+
+                // Init shaders		  
+                initShaders(gl, vs, fs);
+
+                // Draw a point
+                gl.drawArrays(gl.POINTS, 0, 1);
+            });
+        });
+
+    }
+
+    function initShaders(gl, vs_source, fs_source) {
+        // Compile shaders
+        var vertexShader = makeShader(gl, vs_source, gl.VERTEX_SHADER);
+        var fragmentShader = makeShader(gl, fs_source, gl.FRAGMENT_SHADER);
+
+        // Create program
+        var glProgram = gl.createProgram();
+
+        // Attach and link shaders to the program
+        gl.attachShader(glProgram, vertexShader);
+        gl.attachShader(glProgram, fragmentShader);
+        gl.linkProgram(glProgram);
+        if (!gl.getProgramParameter(glProgram, gl.LINK_STATUS)) {
+            alert("Unable to initialize the shader program");
+            return;
+        }
+
+        // Use program
+        gl.useProgram(glProgram);
+    }
+
+    function makeShader(gl, src, type) {
+        var shader = gl.createShader(type);
+        gl.shaderSource(shader, src);
+        gl.compileShader(shader);
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            alert("Error compiling shader: " + gl.getShaderInfoLog(shader));
+            return;
+        }
+        return shader;
+    }
